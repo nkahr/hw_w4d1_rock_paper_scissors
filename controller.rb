1647a8db88ffe210fa4a55dc0af6e9dd28519344
Nina Kahr <ntkahr@gmail.com>
2016-12-13 00:01:16 +0000

    make layout

--- controller.rb
+++ controller.rb
@@ -2,12 +2,12 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('./models/game.rb')
 
-get '/rock_paper_scissors/:action1/:action2' do 
+get '/game/:action1/:action2' do 
   game = Game.new(params[:action1], params[:action2])
   @result = game.return_winner()
   return erb(:result)
 end 
 
-get '/' do 
+get '/game' do 
   erb(:home)
 end 
